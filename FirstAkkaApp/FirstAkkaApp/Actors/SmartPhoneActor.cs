f45f5d74aff0db46befa130e61e97009d5a2959b
Joaquin Garcia Piñero <joaquin.garcia@orenesgrupo.com>
2020-03-28 20:41:30 +0100

    feat: first akka example application

---
+++ FirstAkkaApp/FirstAkkaApp/Actors/SmartPhoneActor.cs
@@ -0,0 +1,15 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstAkkaApp.Actors
+{
+    public class SmartPhoneActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            Console.WriteLine($"SmartPhoneActor Receive a message: {message}");
+        }
+    }
+}
---
+++ FirstAkkaApp/FirstAkkaApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Akka.Actor;
+using FirstAkkaApp.Actors;
+using System;
+
+namespace FirstAkkaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instanciamos un sistema de actores
+            var actorSystem = ActorSystem.Create("SmartPhoneActorSystem");
+
+            // Desplegamos en el sistema de actores un nuevo Actor y nos quedamos con su referencia
+            var smartPhoneActor = actorSystem.ActorOf<SmartPhoneActor>("smartPhoneActorOne");
+
+            // Enviamos al actor smartPhoneActorOne un mensaje
+            smartPhoneActor.Tell("Hola! ¿Que tal estas?");
+
+            Console.ReadLine();
+        }
+    }
+}
